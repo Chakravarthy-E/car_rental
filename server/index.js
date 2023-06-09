0581f731fdd0e7c22ad24fd2f8ce055019c12923
Chakravarthy-7 <chakravarthy.8328@gmai.com>
2023-06-09 11:19:44 +0530

    database setup completed

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log("hey")
+const express = require("express")
+const cors = require("cors")
+require("./db/dbconnection")
+
+const app = express()
+const port = 5000
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({extended:true}))
+
+app.listen(port,()=>{
+    console.log(`server running at ${port}`)
+})
